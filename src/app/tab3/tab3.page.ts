b37490169fc0fd1ff5550593a1d6770311b6ca0c
Cesulion <50085330+Cesulion@users.noreply.github.com>
2020-02-17 16:07:19 +0100

    Commit initial de la tab messages

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
-
+messageInput = '';
+messages = [];
+toast: any;
+  constructor(private toastCtrl: ToastController) {}
+  async settingToast() {
+    this.toast = this.toastCtrl.create({
+      message: 't kon ou quoi',
+      duration: 3000,
+      position: 'bottom'
+    });
+    return await this.toast.present();
+  }
+  sendMessage() {
+    if (this.messageInput !== '') {
+      this.messages.push(this.messageInput);
+      this.messageInput = '';
+      console.log('Envoi de message...' + 'message envoyé :' + this.messages[this.messages.length - 1]);
+    } else {
+      this.toast.present();
+    }
+  }
 }
