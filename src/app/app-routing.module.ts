ba9e781ec82ed7496fc67083f0964ceb931a998a
Bempis <bapts.hentz@gmail.com>
2020-02-18 15:58:16 +0100

    intégratation cordova pour test android

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+// @ts-ignore
 import { NgModule } from '@angular/core';
+// @ts-ignore
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'details-annonce',
     loadChildren: () => import('./details-annonce/details-annonce.module').then( m => m.DetailsAnnoncePageModule)
   }
