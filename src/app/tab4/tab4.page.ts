dd4d87d800351f6d030b3d69fa1d42d8a0b0df17
Cesulion <50085330+Cesulion@users.noreply.github.com>
2020-02-26 00:00:53 +0100

    Commit initial de la tab messages

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
+  connected: string;
+  usrInput: any;
+  pwdInput: any;
 
   constructor() {}
+  logUser() {
+    // On mettra ici le script de connexion, en attendant y a ce qui a ....
+    sessionStorage.setItem('loggedUser', this.usrInput);
+    this.connected = sessionStorage.getItem('loggedUser');
+  }
+  checkIfLogged() {
+    return sessionStorage.getItem('loggedUser') != null;
+  }
 
 }
