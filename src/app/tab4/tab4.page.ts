70ebe5c6fff7889ff9e7512d7d2e55c8987ee618
samyh <cesulion@outlook.fr>
2020-02-27 01:45:37 +0100

    Test de connexion à l'API (pas ouf)

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {log} from 'util';
 
 @Component({
   selector: 'app-tab4',
@@ -6,18 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
+  readonly root = 'http://localhost:8000';
+  posts: any;
   connected: string;
   usrInput: any;
   pwdInput: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
   logUser() {
-    // On mettra ici le script de connexion, en attendant y a ce qui a ....
+    console.log('Log button clicked');
+    // On mettra ici le script de connexion, en attendant y a ce qui a .... /**/
     sessionStorage.setItem('loggedUser', this.usrInput);
     this.connected = sessionStorage.getItem('loggedUser');
   }
   checkIfLogged() {
     return sessionStorage.getItem('loggedUser') != null;
+  }
+  testAPI() {
+    this.posts = this.http.get(this.root + '/compte/getCompte/1');
+    console.log('This is what I got !' + this.posts.toString());
   }
 
 }
