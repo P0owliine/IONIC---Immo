28340aeaf3a17541239e5e8cd47fc04ec196cccc
P0owliine <lagrange.pauline@hotmail.fr>
2020-02-27 17:21:22 +0100

    connexion ok .. annonce du compte pas ok

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import {log} from 'util';
+import {Observable} from 'rxjs';
 
 const optionRequete = {
   headers : new HttpHeaders({'Access-Control-Allow-Origin': '*'})
@@ -13,38 +14,47 @@ const optionRequete = {
 })
 
 export class Tab4Page {
-  readonly root = 'http://localhost:8000';
+  readonly root = 'http://localhost/immo-api/public';
   posts: any;
   connected: string;
   userJson: any;
   usrInput: any;
   pwdInput: any;
   data: any;
+  userId: any;
+  annonces: [];
 
-  constructor(private http: HttpClient) {
-    this.testAPI();
-  }
-
+  constructor(private http: HttpClient) {}
   logUser() {
     console.log('Log button clicked');
     this.data = '{"username": ' + this.usrInput + ', "password": ' + this.pwdInput + '}';
     if (this.http.post(this.root + '/compte/login', this.data, optionRequete)) {
       sessionStorage.setItem('loggedUser', this.usrInput);
-      this.http.get(this.root + '/compte/getIdByUsername', optionRequete).subscribe(data => this.userJson = data);
-      JSON.parse(this.userJson);
-      sessionStorage.setItem('loggedId', this.userJson['id']);
+      this.http.get('http://localhost/immo-api/public/compte/getCompteByUsername/Popo', optionRequete).subscribe(result => {
+        this.userJson = result;
+        this.userId = this.userJson[0].id;
+        sessionStorage.setItem('loggedId', this.userId);
+        console.log(sessionStorage.getItem('loggedId'));
+        this.testAPI(this.userId);
+      });
       this.connected = sessionStorage.getItem('loggedUser');
     } else {
       console.log('nope');
     }
   }
   checkIfLogged() {
-    return sessionStorage.getItem('loggedUser') != null;
+    if (sessionStorage.getItem('loggedUser') != null) {
+      return true;
+    } else { return false; }
   }
-  testAPI() {
-    this.http.get(this.root + '/compte/getCompte/4', optionRequete).subscribe(data => {
-      this.posts = data;
-      console.log(data[0].username);
+  testAPI(id) {
+  console.log(id);
+  this.http.get(this.root + '/annonce/getAnnoncesByCompte/' + id, optionRequete).subscribe(data => {
+    console.log(data[0].id + data[0].title);
+    this.annonces = [
+      { id: data[0].id, title: data[0].title }
+    ];
+    console.log(this.annonces);
     }, err => {
       console.log(err);
     });
