bc7ec7589f33e7fbda89c037e4324d256232e57e
Cesulion <50085330+Cesulion@users.noreply.github.com>
2020-02-28 14:30:41 +0100

    Normalement on peut se connecter

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import {log} from 'util';
 import {Observable} from 'rxjs';
-import {forEach} from "@angular-devkit/schematics";
+import {forEach} from '@angular-devkit/schematics';
+import { ToastController } from '@ionic/angular';
 
 const optionRequete = {
   headers : new HttpHeaders({'Access-Control-Allow-Origin': '*'})
@@ -24,34 +25,41 @@ export class Tab4Page {
   data: any;
   userId: any;
   announces: any [];
+  toast: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private toastCtrl: ToastController, private http: HttpClient) {
+    this.settingToast();
+  }
+  async settingToast() {
+    this.toast = this.toastCtrl.create({
+      message: 'Et non',
+      duration: 3000,
+      position: 'bottom'
+    });
+    return await this.toast.present();
+  }
   logUser() {
     console.log('Log button clicked');
     this.data = '{"username": ' + this.usrInput + ', "password": ' + this.pwdInput + '}';
-    if (this.http.post(this.root + '/compte/login', this.data, optionRequete)) {
-      sessionStorage.setItem('loggedUser', this.usrInput);
-      this.http.get(this.root + '/compte/getCompteByUsername/Popo', optionRequete).subscribe(result => {
+    // tslint:disable-next-line:only-arrow-functions
+    this.http.post(this.root + '/compte/login', this.data, optionRequete).subscribe(data => {
+      if (data === 1) {
+        sessionStorage.setItem('loggedUser', this.usrInput);
+        this.http.get(this.root + '/compte/getCompteByUsername/Popo', optionRequete).subscribe(result => {
         this.userJson = result;
         this.userId = this.userJson[0].id;
         sessionStorage.setItem('loggedId', this.userId);
-        this.testAPI(this.userId);
-      });
-      this.connected = sessionStorage.getItem('loggedUser');
-    } else {
-      console.log('nope');
-    }
+        });
+        this.connected = sessionStorage.getItem('loggedUser');
+      } else {
+        console.log('connexion échouée');
+        this.toast.present();
+      }
+    });
   }
   checkIfLogged() {
     if (sessionStorage.getItem('loggedUser') != null) {
       return true;
     } else { return false; }
-  }
-  testAPI(id) {
-  this.http.get(this.root + '/annonce/getAnnoncesByCompte/' + id, optionRequete).subscribe(data => {
-    this.announces = data;
-    }, err => {
-      console.log(err);
-    });
   }
 }
