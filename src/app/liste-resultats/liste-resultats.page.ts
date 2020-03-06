a7f79f9f0b0babfe087ad344ea5c25ae74ed0248
Bempis <bapts.hentz@gmail.com>
2020-03-06 21:33:47 +0100

    retirer recherche avancée filtres

--- src/app/liste-resultats/liste-resultats.page.ts
+++ src/app/liste-resultats/liste-resultats.page.ts
@@ -41,9 +41,6 @@ export class ListeResultatsPage implements OnInit {
     data = this.http.get(url, optionRequete);
     data.subscribe(resultat => {
       this.results = resultat;
-      if (sessionStorage.getItem('filtres')) {
-        this.applyFilters();
-      }
       if (this.results.length === 0) {
         this.noAnnonce.style.display = 'block';
       } else {
@@ -52,64 +49,5 @@ export class ListeResultatsPage implements OnInit {
         this.nbAnnonces.innerHTML = this.results.length + ' annonces';
       }
     });
-  }
-
-  applyFilters() {
-    if (sessionStorage.getItem('priceMin') !== null || sessionStorage.getItem('priceMax') !== null) {
-      this.checkPrice();
-    }
-    if (sessionStorage.getItem('surfaceMin') !== null || sessionStorage.getItem('surfaceMax') !== null) {
-      this.checkSurface();
-    }
-    if (sessionStorage.getItem('piecesMin') !== null || sessionStorage.getItem('piecesMax') !== null) {
-      this.checkPieces();
-    }
-  }
-
-  checkPrice() {
-    let remove;
-    const priceMin = sessionStorage.getItem('priceMin');
-    const priceMax = sessionStorage.getItem('priceMax');
-    console.log(priceMin + ' ' + priceMax);
-    for (let i = 0; i < this.results.length; i++) {
-      if (priceMin !== null) {
-        if (this.results[i].price < priceMin) {
-          console.log(this.results[i].price + '<' + priceMin);
-          remove = true;
-        }
-      }
-      if (priceMax !== null) {
-        if (this.results[i].price > priceMax) {
-          console.log('max ' + i);
-          remove = true;
-        }
-      }
-
-      if(remove){
-        this.results.splice(i, 1);
-      }
-
-    }
-  }
-
-  checkSurface() {
-    const surfaceMin = sessionStorage.getItem('surfaceMin');
-    const surfaceMax = sessionStorage.getItem('surfaceMax');
-    for (let i = 0; i < this.results.length; i++) {
-      if ((surfaceMin !== null && this.results[i].surface < surfaceMin) || (surfaceMax !== null && this.results[i].surface > surfaceMax) ) {
-        this.results.splice(i, 1);
-      }
-    }
-  }
-
-  checkPieces() {
-    const piecesMin = sessionStorage.getItem('piecesMin');
-    const piecesMax = sessionStorage.getItem('piecesMax');
-    for (let i = 0; i < this.results.length; i++) {
-      if ((piecesMin !== null && this.results[i].rooms_number < piecesMin)
-          || (piecesMax !== null && this.results[i].rooms_number > piecesMax) ) {
-        this.results.splice(i, 1);
-      }
-    }
   }
 }
